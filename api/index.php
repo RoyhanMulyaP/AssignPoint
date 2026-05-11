45a5606a4b7fa452dabf9b027e3786044d63f7d1
Royhan9947 <pratamaroyhan63@gmail.com>
2026-05-12 00:12:13 +0700

    Enable PHP extensions and update runtime for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-// Masuk ke file index utama CI4
+// Pastikan error ditampilkan jika terjadi masalah saat build/runtime
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Definisikan path ke public/index.php
 require __DIR__ . '/../public/index.php';
