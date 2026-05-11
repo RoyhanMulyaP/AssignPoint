c18c707466ecc89788674ba642492171515542e7
Royhan9947 <pratamaroyhan63@gmail.com>
2026-05-12 00:48:40 +0700

    menghapus filde dan script yang berhubungan dengan vercel

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Pastikan error ditampilkan jika terjadi masalah saat build/runtime
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-// Definisikan path ke public/index.php
-require __DIR__ . '/../public/index.php';
